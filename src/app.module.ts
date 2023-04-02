32c69bb604847b50d8186ee969dc112e50f1c55a
vinh <vinh@scalapay.com>
2023-04-02 13:46:27 +1000

    backbone-architecture: set up nestjs, mongoose, docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './schemas/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DATABASE_URL)
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
